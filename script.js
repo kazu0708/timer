6d59fca90d822066519c8e3addccc3aa9aae58a1
みそしる <sasaryo9125@gmail.com>
2024-08-11 16:32:25 +0900

    JavaScript雛型

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    //HTMLのid値を使って以下のDOM要素を取得
+    const hour = document.getElementById("hour");
+    const min = document.getElementById("min");
+    const sec = document.getElementById("sec");
+    const downbutton = document.getElementById('down');
+    const upbutton = document.getElementById('up');
+    const start = document.getElementById('startbutton');
+    const stop = document.getElementById('stopbutton');
+    const reset = document.getElementById('resetbutton');
+});
