666585a60b67b77a8e6671ba934d2b7f7e45f575
みそしる <sasaryo9125@gmail.com>
2024-08-16 13:41:22 +0900

    JavaScript修正

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hour = document.getElementById("hour");
-    const min = document.getElementById("min");
-    const sec = document.getElementById("sec");
-    const downbutton = document.getElementById('down');
-    const upbutton = document.getElementById('up');
+    const min = document.getElementById("minute");
+    const sec = document.getElementById("second");
     const hdown = document.getElementById('hourdownbutton');
     const hup = document.getElementById('hourupbutton');
     const mdown = document.getElementById('minutedownbutton');
@@ -14,5 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const stop = document.getElementById('stop');
     const reset = document.getElementById('reset');
 
-
+    const countdown = () => {
+        if (hour == 0 && minute == 0) {
+            sec -= 1;
+        }
+    }
 });
